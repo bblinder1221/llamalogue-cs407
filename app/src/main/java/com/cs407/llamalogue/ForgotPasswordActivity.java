c3779a650f82792e0b67d4500ea78ea766aa9aee
vuongjennie <jennievuong080502@gmail.com>
2023-12-10 17:01:23 -0600

    Added "Back to Login" button for Forgot Password screen

--- app/src/main/java/com/cs407/llamalogue/ForgotPasswordActivity.java
+++ app/src/main/java/com/cs407/llamalogue/ForgotPasswordActivity.java
@@ -2,13 +2,28 @@ package com.cs407.llamalogue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    public void goToActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        TextView loginRedirect = findViewById(R.id.loginRedirect);
+        loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActivity();
+            }
+        });
     }
 }
